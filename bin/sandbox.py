f8fd1720137777e0a5da661d4128d90887577b49
Austin Lee <austinlee1989@gmail.com>
2017-04-12 17:40:46 -0700

    Added in function for magnitude of change in acceleration from previous second

--- bin/sandbox.py
+++ bin/sandbox.py
@@ -1,10 +1,12 @@
 import MySQLdb
 import pandas as pd
+import math
 
 """
 First task is to import chalkprint data from MySQL DB into a pandas DF, from there build models classifying when
 a user is actually climbing. Introduce magnitude of change of position and net acceleration, find when acceleration == 0
-then we know a user is falling/dropping.
+then we know a user is falling/dropping. Set the height climbed = max(height before accel = 0) - min(height at climb start)
+time logic is set using the same logic
 """
 
 
@@ -23,6 +25,21 @@ del data['timeStamp']
 
 grouped = data.groupby('session_id')
 
-print(grouped.groups)
+"""Create new columns for change in acceleration and change in rotation, group by session_id"""
 
-grouped['relAltitude'].plot()
+def find_diff(data_column):
+        return data_column - data_column.shift(-1)
+
+var_to_loop = ["accelX", "accelY", "accelZ"]
+
+data['delta_accel'] = pd.Series(
+    (sum([find_diff(data[i])**2 for i in var_to_loop]))**.5/len(var_to_loop)
+    , index=data.index
+)
+
+data['delta_accelX'] = pd.Series(find_diff(data['accelX'])**2, index=data.index)
+data['delta_accelY'] = pd.Series(find_diff(data['accelY'])**2, index=data.index)
+data['delta_accelZ'] = pd.Series(find_diff(data['accelZ'])**2, index=data.index)
+
+
+
